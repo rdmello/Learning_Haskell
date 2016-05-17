aa37759ad5e502a68cad4670c95dc1d5b5e8409a
Rylan Dmello <rylandmello@gmail.com>
2016-05-17 18:13:46 -0500

    I/O operations

--- helloword/helloworld.hs
+++ helloword/helloworld.hs
@@ -1,14 +1,39 @@
 -- Rylan Dmello 15 May 2016
 -- Haskell Hello World
 
-import qualified Data.Char as DC
+import Data.Char 
+import Control.Monad
 
 main = do
-  putStrLn "hello, world!"
-  putStrLn "What is your name?"
-  name <- getLine
-  let bigName = map DC.toUpper name
-  putStrLn ("Hey " ++ bigName ++ ", you rock!")
+  colors <- forM [1,2,3,4] (\a -> do
+    putStrLn $ "Which color do you associate with the number " ++ show a ++ "?"
+    color <- getLine
+    return color)
+  putStrLn "The colors that you associate with 1, 2, 3, and 4 are: "
+  mapM putStrLn colors
+  -- putStr "Give me some imput: "
+  -- l <- getLine
+  -- putStrLn $ map toUpper l
+  -- print "hello, world!"
+  -- putStrLn "What is your name?"
+  -- name <- getLine
+  -- let bigName = map DC.toUpper name
+  -- putStr' ("Hey ")
+  -- putStr' (bigName) 
+  -- putStrLn (", you rock!")
+  -- c <- getChar
+  -- when (c /= ' ') $ do
+  --   putChar c
+  --   main
+  -- rs <- sequence [getLine, getLine, getLine]
+  -- print rs
+
+
+putStr' :: String -> IO ()
+putStr' [] = return ()
+putStr' (x:xs) = do
+  putChar x 
+  putStr' xs
 
 -- Doing `:t putStrLn "asdf"` in GHCI gives "asdf" :: IO ()
 -- This is an I/O action, with a result of empty tuple ().
--- reverse_words/reverse_words.hs
+++ reverse_words/reverse_words.hs
@@ -3,7 +3,7 @@
 main = do
   line <- getLine
   if null line
-    then return ()
+    then do return () -- Return is like an inverse of the `<-` operator
     else do
       putStrLn $ reverseWords line
       main

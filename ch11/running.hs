7bb79759704406a6213f27ff78de9dc8f6aa33b2
Rylan Dmello <rylandmello@gmail.com>
2016-05-24 11:30:26 -0500

    Completed newtype section

--- ch11/running.hs
+++ ch11/running.hs
@@ -1,3 +1,5 @@
+import Control.Applicative
+
 -- main = do line' <- fmap reverse getLine
 --           putStrLn $ "You said " ++ line' ++ " backwards!"
 --           putStrLn $ "Yes, you really said " ++ line' ++ " backwards!"
@@ -18,5 +20,35 @@ instance Functor CMaybe where
 myAction :: IO String
 myAction = (++) <$> getLine <*> getLine
 
+sequenceA' :: (Applicative f) => [f a] -> f [a]
+sequenceA' = foldr (liftA2 (:)) (pure [])
 
+-- Newtypes etc
 
+newtype ZipList' a = ZipList' {getZipList :: [a]}
+
+newtype CharList = CharList {getCharList :: [Char]} deriving (Eq, Show)
+
+newtype Pair b a = Pair { getPair :: (a,b) } deriving (Show)
+
+instance Functor (Pair c) where
+  fmap f (Pair (x,y)) = Pair (f x, y)
+
+newtype CoolBool = CoolBool { getCoolBool :: Bool} deriving (Show)
+
+helloMe :: CoolBool -> String
+helloMe (CoolBool _) = "Hello!"
+
+-- Difference between type, data, and newtype
+
+-- TYPE: used for making type synonyms eg
+type IntList = [Int]
+-- IntList can now basically be an alias for [Int]. 
+-- Look at PhoneBook from global running.hs for another example.
+
+-- NEWTYPE is for taking existing types and wrapping in new types
+-- this makes it easier to make them instances of other classes
+-- See CharList example above
+
+-- DATA is for making your own data types. It is the most powerful 
+-- of the three. 
